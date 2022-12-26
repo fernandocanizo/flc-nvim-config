5613bf50b476b9bdd9737e1f4f9af8db08425d57
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-26 19:38:40 -0300

    Refactor and reindent `numToStr/Comment` setup

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,35 +1,30 @@
 -- Setup https://github.com/numToStr/Comment.nvim
 -- See `:h comment-nvim`
 require('Comment').setup({
-    -- Ignore empty lines
-    ignore = '^$',
-    mappings = {
-        -- Disable basic mappings. I'm gonna do my own, see below
-        basic = false,
-        -- Disable extra mappings, they are stupid
-        extra = false,
-    },
+  -- Ignore empty lines
+  ignore = '^$',
+  mappings = {
+    -- Disable basic mappings. I'm gonna do my own, see below
+    basic = false,
+    -- Disable extra mappings, they are stupid
+    extra = false,
+  },
 })
 
 -- Toggle current line or with count
 vim.keymap.set('n', '<F1>', function()
   local toggle_current_line = '<Plug>(comment_toggle_linewise_current)j'
   local toggle_count_lines = '<Plug>(comment_toggle_linewise_count)' .. vim.v.count .. 'j'
-  return vim.v.count == 0 and toggle_current_line
-    or toggle_count_lines
+  return vim.v.count == 0 and toggle_current_line or toggle_count_lines
 end, { expr = true })
 
 -- Toggle visual, line comment
-vim.keymap.set('x', '<F1>', '<Plug>(comment_toggle_linewise_visual)')
+vim.keymap.set('x', '<F1>', '<Plug>(comment_toggle_linewise_visual)', { expr = true })
 
 -- Toggle count, block comment
 vim.keymap.set('n', '<F2>', function()
-  local toggle_count_block = '<Plug>(comment_toggle_blockwise_count)' .. vim.v.count .. 'j'
-  return toggle_count_block
+  return '<Plug>(comment_toggle_blockwise_count)' .. vim.v.count .. 'j'
 end, { expr = true })
 
 -- Toggle visual, block comment
-vim.keymap.set('x', '<F2>', function()
-  local toggle_visual_block = '<Plug>(comment_toggle_blockwise_visual)'
-  return toggle_visual_block
-end, { expr = true })
+vim.keymap.set('x', '<F2>', '<Plug>(comment_toggle_blockwise_visual)', { expr = true })
