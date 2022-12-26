2f5b00642eb931bb66b1da211ea475344520e3a5
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-26 04:17:04 -0300

    Setup cursor line highlight

--- lua/style.lua
+++ lua/style.lua
@@ -1,3 +1,6 @@
 -- Set colorscheme
 vim.o.termguicolors = true
 vim.cmd [[colorscheme onedark]]
+
+vim.cmd('hi CursorLine cterm=NONE ctermbg=darkgrey ctermfg=white guibg=darkgrey guifg=white')
+vim.opt.cursorline = true
