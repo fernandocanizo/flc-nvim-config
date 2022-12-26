3a0d93b349833b83aaeaa68d2b89bf40899a28b2
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-25 21:16:45 -0300

    Remove autocompilation code
    It eats RAM.
    See https://github.com/nvim-lua/kickstart.nvim/issues/89

--- init.lua
+++ init.lua
@@ -84,14 +84,6 @@ if is_bootstrap then
   return
 end
 
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
-
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
