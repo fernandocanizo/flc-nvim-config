b7c4166ed135490d9ca2768a282df6c9a1af78da
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-26 04:55:25 -0300

    Start with cursorline disabled
    Maybe I need to find better colors, but right now it has become a
    nuisance. However I'd like to keep the mapping for toggling it around.

--- lua/style.lua
+++ lua/style.lua
@@ -3,4 +3,4 @@ vim.o.termguicolors = true
 vim.cmd [[colorscheme onedark]]
 
 vim.cmd('hi CursorLine cterm=NONE ctermbg=darkgrey ctermfg=white guibg=darkgrey guifg=white')
-vim.opt.cursorline = true
+vim.opt.cursorline = false
