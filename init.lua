6de3a5d77fe0f795672d54c8ac22aabeac2e1b49
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-26 03:46:51 -0300

    Move general keymaps into a separate module

--- init.lua
+++ init.lua
@@ -13,21 +13,7 @@ end
 
 require('general-settings')
 require('style')
-
--- [[ Basic Keymaps ]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+require('general-keymappings')
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
