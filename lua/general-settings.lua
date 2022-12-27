c2ec9885183259ced341674fa42d36ffc0a8504b
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-27 15:56:53 -0300

    Define default tab/space/indenting settings

--- lua/general-settings.lua
+++ lua/general-settings.lua
@@ -27,3 +27,17 @@ vim.wo.signcolumn = 'yes'
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
+-- Indentation block
+local spaceAmount = 4
+-- use TABs
+vim.o.expandtab = false
+-- number of spaces to show for a TAB character
+vim.o.tabstop = spaceAmount
+-- number of spaces for indent function (>>)
+vim.o.shiftwidth = spaceAmount
+-- When shifting lines, round the indentation to the nearest
+-- multiple of `shiftwidth`
+vim.o.shiftround = true
+-- New lines inherit the indentation of previous lines
+vim.o.autoindent = true
+-- Indentation block ends
