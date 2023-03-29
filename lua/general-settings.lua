6888c46eb51d47783f310907f7f24030baeab3ae
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-03-29 11:14:42 -0300

    Add comment to recall when is `shiftwidth` used

--- lua/general-settings.lua
+++ lua/general-settings.lua
@@ -33,7 +33,7 @@ local spaceAmount = 4
 vim.o.expandtab = false
 -- number of spaces to show for a TAB character
 vim.o.tabstop = spaceAmount
--- number of spaces for indent function (>>)
+-- number of spaces for indent function (>> and ENTER)
 vim.o.shiftwidth = spaceAmount
 -- When shifting lines, round the indentation to the nearest
 -- multiple of `shiftwidth`
