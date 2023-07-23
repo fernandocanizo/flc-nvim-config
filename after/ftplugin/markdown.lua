6e5f9698ca7a959e297e28a45f20dbe922a78e44
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-07-22 21:11:36 -0400

    Set several things for Markdown files
    In particular I wanted to write paragraphs in a single line, but to see
    them visually wrapped.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,18 @@
-vim.o.textwidth = 78
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
+
+-- show wrapped lines visually but don't hard-wrap them
+vim.o.number = true -- just to help visualize this
+vim.o.textwidth = 0
+vim.o.wrapmargin = 0
+-- visual wrap (no real line cutting is made)
+vim.o.wrap = true
+vim.o.linebreak = true -- breaks by word rather than character
+-- next is the magic line: tell nvim that we have only 80 columns
+-- (usually this is set automatically to the wisth of your window)
+vim.opt_local.columns = 80
+
+-- don't wrap around file when searching
+vim.o.wrapscan = false
