5572068417d49d61e9bf606f3b4274dd64596174
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-08-03 19:49:25 -0400

    Add YAML ftplugin
    YAML doesn't work with TAB characters, so ensure we have spaces.

---
+++ after/ftplugin/yaml.lua
@@ -0,0 +1,17 @@
+-- use spaces character when TAB key is pressed
+vim.o.expandtab = true
+
+-- number of spaces to show for a TAB
+vim.o.tabstop = 2
+
+-- number of spaces for indent with >>
+vim.o.shiftwidth = 2
+
+-- number of spaces for a tab in editing operations
+vim.o.softtabstop = 2
+
+vim.o.autoindent = true
+vim.o.smartindent = false
+
+-- no hard wrapping
+vim.o.textwidth = 0
