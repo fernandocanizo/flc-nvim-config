f032031acba9edeaaa11c244615e9a8ed117ce3d
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-08-05 14:23:45 -0400

    Map Telescope `find_files` under `<c-p>`
    I use to use `ctrlp` plugin, so my finger are used to that mapping.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -25,7 +25,11 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]how [F]iles' })
+-- also make it available under CTRL-P, as I used to use
+-- https://github.com/kien/ctrlp.vim plugin
+vim.keymap.set('n', '<c-p>', require('telescope.builtin').find_files, { desc = '[S]how [F]iles' })
+
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
