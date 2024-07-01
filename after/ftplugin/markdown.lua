d7b9d756aefa1794f0f45c1174844866585b829a
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-07-01 10:39:14 -0300

    Disable line width for Markdown files
    Until I find a solution for the vertical split problem.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -11,8 +11,14 @@ vim.o.wrapmargin = 0
 vim.o.wrap = true
 vim.o.linebreak = true -- breaks by word rather than character
 -- next is the magic line: tell nvim that we have only 80 columns
--- (usually this is set automatically to the wisth of your window)
-vim.opt_local.columns = 80
+-- (usually this is set automatically to the width of your window)
+--
+-- TODO this is working wonderful until I have to make a vertical split, then is
+-- a mess because the splits open inside this 80 columns restriction, making the
+-- reading and editing cumbersome. I'll leave it commented, already asked on
+-- Neovim Discourse forum
+--
+-- vim.opt_local.columns = 80
 
 -- don't wrap around file when searching
 vim.o.wrapscan = false
