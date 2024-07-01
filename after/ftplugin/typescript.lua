d92a7d2b176b4c9b4e006cf531717181b0293ff9
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-07-01 10:38:18 -0300

    Indent and spaces configuration for Typescript files

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,1 +1,16 @@
-vim.o.tabstop = 4
+-- Indentation block
+local spaceAmount = 2
+-- use TABs
+vim.o.expandtab = true
+-- number of spaces to show for a TAB character
+vim.o.tabstop = spaceAmount
+-- number of spaces for indent function (>> and ENTER)
+vim.o.shiftwidth = spaceAmount
+-- When shifting lines, round the indentation to the nearest
+-- multiple of `shiftwidth`
+vim.o.shiftround = true
+-- Disable softabstop, which isn't needed
+vim.o.softtabstop = 0
+-- New lines inherit the indentation of previous lines
+vim.o.autoindent = true
+-- Indentation block ends
